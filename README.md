a22d224b8b1da0cc8610dadc0888b388285d5cf5
Kyle Givler <xvegan88x@gmail.com>
2021-07-24 12:49:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RotatorMVC
+ 
---
+++ RotatorMVC/Models/Palindrome.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RotatorMVC.Models
+{
+    public class Palindrome
+    {
+        public string InputWord { get; set; }
+        public string ReversedWord { get; set; }
+        public bool IsPalindrom { get; set; }
+        public string Message { get; set; }
+    }
+}
